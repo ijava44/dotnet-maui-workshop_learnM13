a048dc151aa851c69bb31251882985b26a026410
ijava44 <ischulz105482@gmail.com>
2024-05-26 08:22:24 -0500

    up to date may 26 2024

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -2,4 +2,70 @@
 
 public class BaseViewModel
 {
+
+}
+```
+
+to this
+
+```csharp
+public class BaseViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+}
+public void OnPropertyChanged([CallerMemberName] string name = null) =>
+    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+public class BaseViewModel : INotifyPropertyChanged
+{
+    bool isBusy;
+    string title;
+    //...
+}
+public class BaseViewModel : INotifyPropertyChanged
+{
+    //...
+    public bool IsBusy
+    {
+        get => isBusy;
+        set
+        {
+            if (isBusy == value)
+                return;
+            isBusy = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Title
+    {
+        get => title;
+        set
+        {
+            if (title == value)
+                return;
+            title = value;
+            OnPropertyChanged();
+        }
+    }
+    //...
+}
+public class BaseViewModel : INotifyPropertyChanged
+{
+    //...
+    public bool IsBusy
+    {
+        get => isBusy;
+        set
+        {
+            if (isBusy == value)
+                return;
+            isBusy = value;
+            OnPropertyChanged();
+            // Also raise the IsNotBusy property changed
+            OnPropertyChanged(nameof(IsNotBusy));
+        }
+    }
+
+    public bool IsNotBusy => !IsBusy;
+    //...
 }
